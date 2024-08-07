2bbc8f2cd5d44b4c2b13e79e06ea7470d462cac9
TruongQuangThuanThien <sthiennz@gmail.com>
2024-08-07 13:47:54 +0700

    Buoi5_BT03

---
+++ Buoi_5/Buoi_5/Buoi5_BT03.cpp
@@ -0,0 +1,24 @@
+﻿#include<stdio.h>
+
+// Hàm đệ quy để đếm số chữ số của số nguyên dương n
+int countDigits(int n) {
+    if (n == 0)
+        return 0;
+    return 1 + countDigits(n / 10);
+}
+
+int main() {
+    int n;
+    printf("Nhap so nguyen duong n: ");
+    scanf_s("%d", &n);
+
+    if (n <= 0) {
+        printf("So nhap vao phai la so nguyen duong.\n");
+        return 1;
+    }
+
+    int count = countDigits(n);
+    printf("So chu so cua %d la: %d\n", n, count);
+
+    return 0;
+}
