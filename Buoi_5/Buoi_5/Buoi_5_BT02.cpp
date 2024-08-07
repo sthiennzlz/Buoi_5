4c1bb0c852da0d278fbcac0ad85344e345cfb2a5
TruongQuangThuanThien <sthiennz@gmail.com>
2024-08-07 13:24:09 +0700

    Buoi5_BT02_a Tính tổng các phần tử chẵn của a.

---
+++ Buoi_5/Buoi_5/Buoi_5_BT02.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+//a Hàm tính tổng các phần tử chẵn trong mảng
+int sumEven(int a[], int n) {
+    if (n == 0) return 0;
+    int last = a[n - 1];
+    if (last % 2 == 0)
+        return last + sumEven(a, n - 1);
+    else
+        return sumEven(a, n - 1);
+}//
+
+int main() {
+    int a[] = { 3, 6, 8, 5, 10, 13, 2 };
+    int n = sizeof(a) / sizeof(a[0]);
+
+    printf("Tong cac phan tu chan: %d\n", sumEven(a, n));
+    return 0;
+}
